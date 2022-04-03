bad975ae1c65f1f0f4d4bedba3864933689fb0e0
xaykz <102754951+1099xaykz@users.noreply.github.com>
2022-04-03 20:12:12 -0300

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(10. / 100)
+
+def main():
+  slowprint("Instalando...")
+  os.system("pip install requests")
+  os.system("py -m install requests")
+  os.system("python -m install requests")
+  print("Instalado")
+  
+if __name__=="__main__":
+  main():
